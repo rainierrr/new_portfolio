364ecc87035ca4fb3270ddbe9f4139756d72c1d6
110665 <yudai.ishiguro110665@e-seikatsu.co.jp>
2021-05-15 22:28:49 +0900

    prettierとESlintを追加

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM node:14-alpine3.10
 WORKDIR /usr/src/app
+RUN apk add --no-cache git
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
-import * as React from "react"
-import { render } from "react-dom"
+import * as React from "react";
+import { render } from "react-dom";
 
-function Root(){
-  return (
-    <div>
-      test4
-    </div>
-  );
+function Root() {
+  return <div>test4</div>;
 }
 
 render(<Root />, document.getElementById("app"));
